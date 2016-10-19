d35fb2551d1a32b0be2e841fa245a58dcecc5a93
jankoju1 <jankoju1@J09-11.uhk.cz>
2016-10-19 16:11:17 +0200

    Založení game třídy

--- src/cz/uhk/fim/pro2/game/Game.java
+++ src/cz/uhk/fim/pro2/game/Game.java
@@ -1,9 +1,10 @@
+package cz.uhk.fim.pro2.game;
 
-public class Program {
+public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi world! :)");
+
 	}
 
 }
